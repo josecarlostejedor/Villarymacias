b4f01bd94b5cd93b904370ba859a40f358637229
josecarlostejedor <josecarlostejedor@gmail.com>
2026-03-26 09:33:22 +0100

    Update main_entry.tsx

--- src/main_entry.tsx
+++ src/main_entry.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from '@/src/App_main';
-import '@/src/index.css';
+import App from './App_main';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
